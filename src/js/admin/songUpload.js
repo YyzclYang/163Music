a850e50672bd9aa7c12fb8601c592f2f7f1c3fbd
YyzclYang <yyzclyang@gmail.com>
2018-09-19 14:58:35 +0800

    <feat>:<完成上传进度条及上传歌曲的显示>

--- src/js/admin/songUpload.js
+++ src/js/admin/songUpload.js
@@ -23,16 +23,16 @@
       </div>
     `,
     liTemplate: `
+      <div class="uploadStatus active">
+        <div class="progressWrapper">
+          <span>上传中……</span>
+          <div class="progressBar">
+          </div>
+        </div>
+      </div>
       <div class="progressName"></div>
       <div class="progressFileSize"></div>
       <div class="progressDetail">
-        <div class="uploadStatus">
-          <div class="progressWrapper">
-            <div class="progressBar">
-            </div>
-          </div>
-          <div class="statusText"></div>
-        </div>
         <div class="fileDetail">
           <div class="singer">
             <strong>Singer:</strong>
@@ -50,6 +50,60 @@
     },
     find(selector) {
       return $(this.el).find(selector)[0];
+    },
+    activeItem(selector) {
+      $(selector)
+        .addClass('active')
+        .siblings()
+        .removeClass('active');
+    },
+    unactiveItem(selector) {
+      $(selector).removeClass('active');
+    },
+    uploadListRender(song) {
+      let liTemplate = this.liTemplate;
+      let li = $(
+        `<li data-qiniuId="${song.qiniuId}" class="active">${liTemplate}</li>`
+      );
+
+      li.find('.progressName').text(song.name);
+
+      $(this.el)
+        .find('ul')
+        .append(li);
+    },
+    updateUpload(uploadInfo) {
+      let uploadPercent = uploadInfo.percent;
+      let size = Math.round(uploadInfo.size / 1024);
+      let curSize = Math.round((size * uploadPercent) / 100);
+      let sizeText;
+
+      //更新文件大小
+      if (size) {
+        if (size > 1024) {
+          sizeText = Math.round((size * 100) / 1024) / 100 + ' Mb';
+        } else {
+          sizeText = size + ' Kb';
+        }
+        $(`li[data-qiniuId = ${uploadInfo.qiniuId}] > .progressFileSize`).text(
+          sizeText
+        );
+      }
+
+      //更新进度条
+      $(
+        `li[data-qiniuId = ${uploadInfo.qiniuId}] .uploadStatus .progressBar `
+      ).css('width', `${uploadPercent}%`);
+
+      //更新文件信息
+      if (uploadInfo.singer || uploadInfo.hash) {
+        $(`li[data-qiniuId = ${uploadInfo.qiniuId}] .singer > span `).text(
+          uploadInfo.singer || ''
+        );
+        $(`li[data-qiniuId = ${uploadInfo.qiniuId}] .hash > span `).text(
+          uploadInfo.hash || ''
+        );
+      }
     }
   };
   let model = {};
@@ -60,6 +114,7 @@
       this.view.render(this.model.data);
       this.initQiniu();
       this.bindEvents();
+      this.bindEventHub();
     },
     initQiniu() {
       //引入Plupload 、qiniu.js后
@@ -82,6 +137,21 @@
           FilesAdded: function(up, files) {
             plupload.each(files, function(file) {
               // 文件添加进队列后,处理相关的事情
+              let res = file.name;
+
+              /* 歌曲文件名格式为 [歌手] - [歌名].格式 */
+              let song = res.split('.');
+              if (song.length > 2) {
+                song.length--;
+                song[0] = song.join('.');
+                song.length = 1;
+              }
+              song = song[0].split(' - ');
+              window.eventHub.trigger('uploadAdd', {
+                qiniuId: file.id,
+                name: song[1] || res,
+                singer: song[0]
+              });
             });
           },
           BeforeUpload: function(up, file) {
@@ -89,9 +159,38 @@
           },
           UploadProgress: function(up, file) {
             // 每个文件上传时,处理相关的事情
+            window.eventHub.trigger('uploading', {
+              qiniuId: file.id,
+              size: file.size,
+              speed: file.speed,
+              percent: file.percent
+            });
           },
           FileUploaded: function(up, file, info) {
             // 每个文件上传成功后,处理相关的事情
+            let res = JSON.parse(info.response);
+            let domain = up.getOption('domain');
+            let sourceLink =
+              'http://' + domain + '/' + encodeURIComponent(res.key);
+            console.log('上传完成');
+
+            /* 歌曲文件名格式为 [歌手] - [歌名].格式 */
+            let song = res.key.split('.');
+            if (song.length > 2) {
+              song.length--;
+              song[0] = song.join('.');
+              song.length = 1;
+            }
+            song = song[0].split(' - ');
+            //给变更上传任务状态用
+            window.eventHub.trigger('uploaded', {
+              qiniuId: file.id,
+              name: song[1] || res.key,
+              singer: song[0],
+              url: sourceLink,
+              hash: res.hash
+            });
+
             // 其中 info.response 是文件上传成功后，服务端返回的json，形式如
             // {
             //    "hash": "Fh8xVqod2MQ1mocfI4S4KpRL6D98",
@@ -123,7 +222,22 @@
 
       // uploader 为一个plupload对象，继承了所有plupload的方法，参考http://plupload.com/docs
     },
-    bindEvents() {}
+    bindEvents() {},
+    bindEventHub() {
+      window.eventHub.on('uploadAdd', songData => {
+        this.view.uploadListRender(songData);
+        this.view.activeItem(`li[data-qiniuId = ${songData.qiniuId}]`);
+      });
+      window.eventHub.on('uploading', uploadInfo => {
+        this.view.updateUpload(uploadInfo);
+      });
+      window.eventHub.on('uploaded', songData => {
+        this.view.updateUpload(songData);
+        this.view.unactiveItem(
+          `li[data-qiniuId = ${songData.qiniuId}] > .uploadStatus`
+        );
+      });
+    }
   };
   controller.init(view, model);
 }
