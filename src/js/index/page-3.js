5220621434f016965a3fc3101ba3006d180087e1
YyzclYang <yyzclyang@gmail.com>
2018-10-11 20:11:12 +0800

    <feat>:<增加实时显示搜索选项>

--- src/js/index/page-3.js
+++ src/js/index/page-3.js
@@ -12,23 +12,79 @@
           <use xlink:href="#icon-close"></use>
         </svg>
       </div>
-      <div class="hotSearch">
+      <div class="hotSearch active">
         <h3>热门搜索</h3>
-        <ul class="hotWord">
-          <li class="word">张杰</li>
-          <li class="word">凉生</li>
-          <li class="word">飘向北方</li>
-          <li class="word">不染</li>
-          <li class="word">国风极乐夜</li>
+        <ul class="hotWords">
+          <li class="hotWord">张杰</li>
+          <li class="hotWord">凉生</li>
+          <li class="hotWord">飘向北方</li>
+          <li class="hotWord">不染</li>
+          <li class="hotWord">国风极乐夜</li>
+        </ul>
+      </div>
+      <div class="searchOptions">
+        <div class="searchTitle"></div>
+        <ul class="searchWords">
+        </ul>
+      </div>
+      <div class="searchResult">
+        <ul>
+        <li>12333</li>
         </ul>
       </div>
     `,
     render(data) {
       $(this.el).html(this.template);
+    },
+    renderSearchWord(data) {
+      let { searchTitle, searchWords } = data;
+      searchTitle = `搜索“${searchTitle}”`;
+      $(this.el)
+        .find('.searchTitle')
+        .text(searchTitle);
+      $(this.el)
+        .find('ul.searchWords')
+        .empty();
+      let liDom = searchWords.map(word => {
+        let $li = $(`
+          <li class="searchWord">
+            <svg class="icon icon-search" aria-hidden="true">
+              <use xlink:href="#icon-search"></use>
+            </svg>
+            <span>${word}</span>
+          </li>
+        `);
+        return $li;
+      });
+      liDom.map(dom => {
+        $(this.el)
+          .find('ul.searchWords')
+          .append(dom);
+      });
+    },
+    renderSearchResult({ searchWord, songs }) {
+      $(this.el)
+        .find(`input[name=search]`)
+        .val(searchWord);
     }
   };
   let model = {
-    data: {}
+    data: {
+      songs: [],
+      searchOptions: {
+        searchTitle: '',
+        searchWords: ['One', 'Two', 'Three']
+      }
+    },
+    find(data) {
+      let query = new AV.Query('Song');
+      return query.find().then(songs => {
+        this.data.songs = songs.map(song => {
+          return { id: song.id, ...song.attributes };
+        });
+        return songs;
+      });
+    }
   };
   let controller = {
     init(view, model) {
@@ -41,17 +97,56 @@
     bindEvents() {
       $('.search').on('input propertychange', () => {
         if ($(`input[name=search]`).val().length > 0) {
-          $('.search').addClass('active');
+          this.model.data.searchOptions.searchTitle = $(
+            `input[name=search]`
+          ).val();
+          window.eventHub.trigger('searching');
         } else {
-          $('.search').removeClass('active');
+          window.eventHub.trigger('clearSearch');
         }
       });
       $('.search > .icon').on('click', () => {
-        $(`input[name=search]`).val('');
-        $('.search').removeClass('active');
+        window.eventHub.trigger('clearSearch');
+      });
+      $('.searchWords').on('click', 'li', event => {
+        let searchWord = event.target.innerText;
+        this.model.data.searchOptions.searchTitle = searchWord;
+        window.eventHub.trigger('searched', { searchWord });
+      });
+      $('input[name=search]').focus(() => {
+        if ($(`input[name=search]`).val().length > 0) {
+          window.eventHub.trigger('searching');
+        }
       });
     },
-    bindEventHub() {}
+    bindEventHub() {
+      window.eventHub.on('searching', () => {
+        $('.search').addClass('active');
+        $('.hotSearch').removeClass('active');
+        //设置搜索提示词
+        $('.searchOptions').addClass('active');
+        this.view.renderSearchWord(this.model.data.searchOptions);
+        $('.searchResult').removeClass('active');
+      });
+      window.eventHub.on('clearSearch', () => {
+        this.model.data.searchOptions.searchTitle = '';
+        $(`input[name=search]`).val('');
+        $('.search').removeClass('active');
+        $('.hotSearch').addClass('active');
+        $('.searchOptions').removeClass('active');
+        $('.searchResult').removeClass('active');
+      });
+      window.eventHub.on('searched', ({ searchWord }) => {
+        this.model.find().then(() => {
+          $('.searchOptions').removeClass('active');
+          $('.searchResult').addClass('active');
+          this.view.renderSearchResult({
+            searchWord,
+            songs: this.model.data.songs
+          });
+        });
+      });
+    }
   };
   controller.init(view, model);
 }
